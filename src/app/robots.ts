3dc3c4c0590db4ce91388f214bafd65727b63b9f
Mari1700-Rage <kemariclark777@gmail.com>
2025-07-20 03:18:30 -0400

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,16 +1,12 @@
+import { MetadataRoute } from 'next';
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/private/',
-      },
-      {
-        userAgent: 'Googlebot-Image',
-        allow: '/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
     sitemap: 'https://eraseto.com/sitemap.xml',
   };
-}
+} 
