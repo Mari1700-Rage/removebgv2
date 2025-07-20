a955f258d5dde2457e94747609f6c72acf6a0030
Mari1700-Rage <kemariclark777@gmail.com>
2025-07-19 21:16:18 -0400

    Update page.tsx

--- src/app/(site)/faqs/page.tsx
+++ src/app/(site)/faqs/page.tsx
@@ -196,6 +196,8 @@ export default function FAQsPage() {
                                         : 'bg-gray-700 border border-gray-600 text-white hover:bg-gray-600'
                                 }`}
                             >
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4619589162374260"
+     crossorigin="anonymous"></script>
                                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                 </svg>
