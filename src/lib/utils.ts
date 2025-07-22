7d90f45c2883b51f62290165b9ae002e24f1aa08
Mari1700-Rage <kemariclark777@gmail.com>
2025-07-22 02:49:50 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,17 +2,17 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function getSizeTrans(fs: number): string {
-	if (fs < 1024) {
-		return String(fs);
-	} else if (fs < 1024 * 1024) {
-		return parseInt(String((fs * 10) / 1024)) / 10 + " KB";
-	} else if (fs < 1024 * 1024 * 1024) {
-		return parseInt(String((fs * 10) / 1024 / 1024)) / 10 + " MB";
-	} else {
-		return parseInt(String((fs * 10) / 1024 / 1024 / 1024)) / 10 + " GB";
-	}
+  if (fs < 1024) {
+    return fs + " bytes";
+  } else if (fs < 1024 * 1024) {
+    return Math.round((fs / 1024) * 10) / 10 + " KB";
+  } else if (fs < 1024 * 1024 * 1024) {
+    return Math.round((fs / (1024 * 1024)) * 10) / 10 + " MB";
+  } else {
+    return Math.round((fs / (1024 * 1024 * 1024)) * 10) / 10 + " GB";
+  }
 }
