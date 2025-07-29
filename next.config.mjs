1d4d05b8541435c6f157c1fbb3930985938d517f
Mari1700-Rage <kemariclark777@gmail.com>
2025-07-29 02:58:45 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,10 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-eval' https://pagead2.googlesyndication.com https://www.googletagservices.com https://ep2.adtrafficquality.google",
+      "script-src 'self' https://pagead2.googlesyndication.com https://www.googletagservices.com https://ep2.adtrafficquality.google 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline'",
-      "img-src * data:",
-      "connect-src 'self' blob: data: https://ep2.adtrafficquality.google",
+      "img-src 'self' data: https://*",
+      "connect-src 'self' https://* blob: data:",
       "worker-src 'self' blob:",
       "frame-src https://*.doubleclick.net https://*.google.com https://*.googlesyndication.com",
       "child-src https://*.doubleclick.net https://*.google.com https://*.googlesyndication.com",
