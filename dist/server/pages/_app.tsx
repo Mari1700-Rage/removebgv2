78ace5abc5b30dfbd6519412e7883ff63fd593da
Mari1700-Rage <kemariclark777@gmail.com>
2025-08-09 00:12:03 -0400

    Add files via upload

---
+++ dist/server/pages/_app.tsx
@@ -0,0 +1,25 @@
+// pages/_app.tsx
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [canonicalUrl, setCanonicalUrl] = useState('')
+
+  useEffect(() => {
+    const baseUrl = 'https://eraseto.com'
+    const cleanPath = router.asPath.split('?')[0] // Strip query params
+    setCanonicalUrl(`${baseUrl}${cleanPath}`)
+  }, [router.asPath])
+
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
