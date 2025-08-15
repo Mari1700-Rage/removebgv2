4e2fd1dcb90e1dcea4ee3955fd245c56f225ba8e
Mari1700-Rage <kemariclark777@gmail.com>
2025-08-15 08:49:01 -0400

    Update layout.8d0a73b17acd7470.hot-update.js

--- dist/static/webpack/app/layout.8d0a73b17acd7470.hot-update.js
+++ dist/static/webpack/app/layout.8d0a73b17acd7470.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("app/layout",{
 
 /***/ "(app-pages-browser)/./src/app/globals.css":
 /*!*****************************!*\
-  !*** ./src/app/globals.css ***!
+  !*** ./src/app/globals.css ***! 
   \*****************************/
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
