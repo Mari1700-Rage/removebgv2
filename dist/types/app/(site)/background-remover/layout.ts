a3b58d9a27e207e84ac0499b2c0d069ce0193d06
Mari1700-Rage <kemariclark777@gmail.com>
2025-08-15 08:05:58 -0400

    Update layout.ts

--- dist/types/app/(site)/background-remover/layout.ts
+++ dist/types/app/(site)/background-remover/layout.ts
@@ -2,7 +2,7 @@
 import * as entry from '../../../../../src/app/(site)/background-remover/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../src/app/(site)/background-remover/layout.js')
+type TEntry = typeof import('../../../../../src/app/(site)/background-remover/layout.js') 
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
